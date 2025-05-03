a2af7a3105ef2ef63a13fc370f0375ce25aab822
RMahshie50 <mahshie.r@northeastern.edu>
2025-05-03 12:21:11 -0400

    added website to projects

--- src/components/sections/Projects.jsx
+++ src/components/sections/Projects.jsx
@@ -137,6 +137,34 @@ export const Projects = () => {
                             */}
                     </div>
 
+                    <div className="p-6 rounded-xl border border-white/10 hover:-translate-y-1 hover:border-purple-500/30 
+                                    hover:shadow-[0_2px_8px_rgba(59, 130, 246, 0.1)] transition-all">
+                        <h3 className="text-xl font-bold mb-2"> This Website </h3>
+                        <p className="text-gray-400 mb-4">
+                            I made this website to be a portfolio of my work and a to explore Frontend programming.                       
+                        </p>
+                        <div className="flex flex-wrap gap-2 mb-4">
+                            {["JavaScript", "React", "Tailwind CSS", "Vite", "GitHub Pages", "Static Hosting", "Custom Domain"].map((tech, key) => (
+                                <span 
+                                    key={key}
+                                    className="bg-purple-500/10 text-purple-500 py-1 px-3 rounded-full text-sm hover:bg-purple-500/20
+                                                hover:shadow-[0_2px_8px_rgba(59, 130, 246, 0.2)] transition-all"
+                                >
+                                    {tech} 
+                                </span>
+                            ))}
+                        </div>
+                        {/*
+                        <div className="flex justify-between items-center">
+                            <a 
+                                href="#" 
+                                className="text-purple-400 hover:text-purple 300 transition-colors my-5"
+                            > 
+                                View Project →</a>
+                        </div>
+                            */}
+                    </div>
+
 
 
                 </div>
